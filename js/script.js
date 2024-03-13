99d7bba0f6c75853051f43662231e0df5e83cdbc
Sandra Treesa Tom <109941018+ElizTom@users.noreply.github.com>
2024-03-13 18:28:03 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -4,25 +4,24 @@ import Stats from 'three/addons/libs/stats.module.js';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-import { Vector3 } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 let camera, scene, renderer;
-let stats, glbModel, orbitControls, plane1, plane2, plane3, plane4, plane5, circle, jData, dispText = [];;
-
+let stats, glbModel, dialModel = [], orbitControls, jData, redMaterial, greenMaterial, dialMaterial, glassMaterial;
+let planes = [], textMesh = [], box = [], dialLoaded = false, whiteC, blackC, canedit = true;
 let grid;
 let controls;
 
 function init() {
 
     const container = document.getElementById('container');
-
     renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.shadowMap.enabled = true;
+    renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(render);
@@ -69,14 +68,78 @@ function init() {
     grid.material.opacity = 0.2;
     grid.material.depthWrite = false;
     grid.material.transparent = true;
+    grid.receiveShadow = true;
     scene.add(grid);
 
-    //circle
-    const geometryCirle = new THREE.CircleGeometry(.1, 32);
-    const materialCircle = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    circle = new THREE.Mesh(geometryCirle, materialCircle);
-    circle.position.set(0, 2, 0);
-    scene.add(circle);
+    // const groundGeo = new THREE.PlaneGeometry(10000, 10000);
+    // const groundMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    // groundMat.color.setHSL(0.095, 1, 0.75);
+
+    // const ground = new THREE.Mesh(groundGeo, groundMat);
+    // ground.position.y = 0;
+    // ground.rotation.x = - Math.PI / 2;
+    // ground.receiveShadow = true;
+    // scene.add(ground);
+
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+    // scene.add(ambientLight);
+
+    // const dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    // dirLight.position.set(0, 1000, 0);
+    // scene.add(dirLight);
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 50, 0);
+    scene.add(hemiLight);
+
+    const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+    scene.add(hemiLightHelper);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.color.setHSL(0.1, 1, 0.95);
+    dirLight.position.set(- 1, 1.75, 1);
+    dirLight.position.multiplyScalar(30);
+    scene.add(dirLight);
+
+    dirLight.castShadow = true;
+
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+
+    const d = 10;
+
+    dirLight.shadow.camera.left = - d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = - d;
+
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = - 0.0001;
+
+    // const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 10);
+    // scene.add(dirLightHelper);
+
+
+    whiteC = new THREE.Color(0xffffff);
+    blackC = new THREE.Color(0x000);
+
+    dialMaterial = new THREE.MeshPhysicalMaterial({
+        metalness: 1.0, roughness: 0.5, clearcoat: 1.0, clearcoatRoughness: 0.03
+    });
+
+    glassMaterial = new THREE.MeshPhysicalMaterial({
+        color: 0xffffff, metalness: 0.25, roughness: 0, transmission: 1.0
+    });
+
+    redMaterial = new THREE.MeshStandardMaterial({
+        color: 0xbf3030, metalness: 1.0, roughness: 0.5
+    });
+
+    greenMaterial = new THREE.MeshStandardMaterial({
+        color: 0x5bb73b, metalness: 1.0, roughness: 0.5
+    });
 
     // Model
     const dracoLoader = new DRACOLoader();
@@ -86,64 +149,97 @@ function init() {
     loader.setDRACOLoader(dracoLoader);
 
     loader.load('models/PTC_Aachen.glb', function (gltf) {
-
-        // console.log(gltf);
         glbModel = gltf.scene;
-        // glbModel = gltf.scene.children[0];
-
-        for (let i = 0; i < glbModel.children.length; i++) {
-            // console.log(glbModel.children[i].name);
-        }
         glbModel.scale.set(.7, .7, .7);
+        // glbModel.position.set(0, 0, 0);
         scene.add(glbModel);
-
-
+        glbModel.traverse(function (node) {
+            if (node.isMesh || node.isLight) {
+                node.castShadow = true;
+                node.receiveShadow = true;
+            }
+        })
     });
 
+    creatPlane("plane1", 1.7, 1.7, -5, 1) //CAG
+    creatPlane("plane2", -3, 2.2, 0, 2) //HFL
+    creatPlane("plane3", 0.7, 2, -3.5, 3) //LC
+    creatPlane("plane4", 0, 2.2, -0, 4) //LM
+    creatPlane("plane5", -2.5, 1.4, -3.5, 5) //LV
 
+    for (let i = 0; i < planes.length; i++) {
+        box[i] = [];
+        textMesh[i] = [];
+        dialModel[i] = [];
+    }
+    for (let i = 0; i < planes.length; i++) {
+        createBox(planes[i], -.5, -.3, 0, i, 0);
+        createBox(planes[i], -0, -.3, 0, i, 1);
+        createBox(planes[i], .5, -.3, 0, i, 2);
+
+        loadDial(planes[i], -.5, -.1, 0, "p" + i + "Dial", i, 0)
+        loadDial(planes[i], 0, -.1, 0, "p" + i + "Dial", i, 1)
+        loadDial(planes[i], .5, -.1, 0, "p" + i + "Dial", i, 2)
+
+        const geometry = new THREE.PlaneGeometry(1.5, .2);
+        const material = new THREE.MeshBasicMaterial({ color: 0x000e69, side: THREE.DoubleSide });
+        let blueP1 = new THREE.Mesh(geometry, material);
+        blueP1.position.set(0, .4, 0.01);
+        planes[i].add(blueP1);
+    }
+    setTimeout(() => {
+        ajaxCall();
+    }, 3000);
+}
+
+function loadDial(planeNm, x, y, z, dName, index, dId) {
+    let dial = dName + dId;
+    let modelD = 'models/Dial.glb';
+    if (dId == 2) modelD = 'models/Dial 1.glb';
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('js/Draco/');
+
+    const loader = new GLTFLoader();
+    loader.setDRACOLoader(dracoLoader);
+    loader.load(modelD, function (gltf) {
+        dial = gltf.scene;
+        dial.traverse(function (node) {
+            if (node.isMesh || node.isLight) {
+                node.castShadow = true;
+                node.receiveShadow = false;
+            }
+
+        });
+
+        dial.scale.set(.1, .1, .1);
+        dial.rotation.set(1.55, 0, 0);
+        dial.position.set(x, y, .019);
+        planeNm.add(dial);
+        dialModel[index][dId] = dial;
+    });
+
+}
+
+function creatPlane(planeNm, x, y, z, id) {
     const geometry = new THREE.PlaneGeometry(1.5, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-    plane1 = new THREE.Mesh(geometry, material);
-    plane1.position.set(-3, 2.2, 0);
-    scene.add(plane1);
+    planeNm = new THREE.Mesh(geometry, material);
+    planeNm.position.set(x, y, z);
+    planeNm.castShadow = true;
+    planeNm.receiveShadow = true;
+    scene.add(planeNm);
+    planes[id - 1] = planeNm;
+}
 
-    plane2 = new THREE.Mesh(geometry, material);
-    plane2.position.set(2, 2.2, -2);
-    scene.add(plane2);
-    plane3 = new THREE.Mesh(geometry, material);
-    plane3.position.set(2, 2.2, -2);
-    scene.add(plane3);
-    plane4 = new THREE.Mesh(geometry, material);
-    plane4.position.set(2, 2.2, -2);
-    scene.add(plane4);
-    plane5 = new THREE.Mesh(geometry, material);
-    plane5.position.set(2, 2.2, -2);
-    scene.add(plane5);
-
-    ajaxCall();
-
-    // fontLoad.load('./fonts/helvetiker_regular.typeface.json', function (font) {
-    //     for (let i = 0; i <= 4; i++) {
-    //         dispText[i] = "";
-    //         for (var key in jData.rows[i]) {
-    //             dispText[i] += key + " : " + jData.rows[i][key] + "\n";
-    //         }
-    //     }
-    //     const geometry = new TextGeometry(dispText[1], {
-    //         font: font,
-    //         size: .08,
-    //         height: .001
-    //     });
-    //     const materials = [
-    //         new THREE.MeshPhongMaterial({ color: 0x000 }), // front
-    //         new THREE.MeshPhongMaterial({ color: 0x000 }) // side
-    //     ];
-    //     const textMesh1 = new THREE.Mesh(geometry, materials);
-    //     textMesh1.position.set(-.7, .2, 0);
-    //     textMesh1.textAlign = 'center';
-    //     plane1.add(textMesh1)
-    // });
-
+function createBox(planeName, x, y, z, index, bId) {
+    let boxName = index + "Box" + bId;
+    const geometry = new THREE.BoxGeometry(.13, .07, .001);
+    const material = new THREE.MeshBasicMaterial({ color: 0x5bb73b });
+    geometry.center();
+    boxName = new THREE.Mesh(geometry, material);
+    boxName.position.set(x, y, z);
+    planeName.add(boxName);
+    box[index][bId] = boxName;
 }
 
 function onWindowResize() {
@@ -154,20 +250,44 @@ function onWindowResize() {
 }
 
 function render() {
-    plane1.lookAt(camera.position);
-    plane2.lookAt(camera.position);
-    circle.lookAt(camera.position);
+    // console.log("iS")
+    for (let i = 0; i < planes.length; i++) {
+        planes[i].lookAt(camera.position)
+    }
     controls.update();
     renderer.render(scene, camera);
     stats.update();
-    // console.log(camera.position);
 }
 
 setInterval(ajaxCall, 30000);
+// setInterval(dialFn, 5000);
 init();
+function dialFn() {
+    if (dialModel[0][0]) {
+        // console.log(dialModel[0][0].getObjectByName('Needle'))
+        console.log(dialModel[0][0].getObjectByName('Needle').material)
+
+        for (let i = 0; i < dialModel[0][0].children.length; i++) {
+            // console.log(dialModel[0][0].children[i].name)
+            // dialModel[i][j].getObjectByName('Needle').children[1].material = redMaterial
+            //     if (dialModel[0][0].children[i].children.length > 0) {
+            //         for (let j = 0; j < dialModel[0][0].children[i].children.length; j++) {
+            //             // dialModel[0][0].children[i].material
+
+            //             dialModel[0][0].children[i].children[j].material = dialMaterial
+            //         }
+            //     }
+            //     else {
+            //         dialModel[0][0].children[i].material = dialMaterial
+            //     }
+            //     dialModel[0][0].castShadow = true
+        }
+    }
+}
 
 function ajaxCall() {
     console.log("ajax")
+
     $.ajax({
         url: 'https://dfa-twx.germanywestcentral.cloudapp.azure.com/Thingworx/Things/LTTS.ESM.Manager/Services/GetAllAssetDataFor3D',
         crossDomain: true,
@@ -179,32 +299,94 @@ function ajaxCall() {
         contentType: 'application/json'
     }).done(function (response) {
         jData = response;
-        console.log("jData", jData);
-        console.log("jData", jData.rows[0]);
 
-        // for (let i = 0; i <= 4; i++) {
-        //     dispText[i] = "";
-        //     for (var key in jData.rows[i]) {
-        //         // console.log(key + " : " + jData.rows[i][key]);
-        //         dispText[i] += key + " : " + jData.rows[i][key] + "\n";
-        //     }
-        // }
+        for (let i = 0; i < planes.length; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (j > 3) {
+                    planes[i].remove(textMesh[i][j]);
+                }
+            }
+        }
+
+        for (let i = 0; i < planes.length; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (dialModel[i][j]) {
+                    dialLoaded = true;
+                    if (j == 0) {
+                        if (jData.rows[i].Current > 50) {
+                            dialModel[i][j].getObjectByName('Needle').material = redMaterial;
+                            box[i][j].material.color = new THREE.Color(0xbf3030);
+                        } else {
+                            dialModel[i][j].getObjectByName('Needle').material = greenMaterial;
+                            box[i][j].material.color = new THREE.Color(0x5bb73b);
+                        }
+                        dialModel[i][j].getObjectByName('Needle').rotation.y = jData.rows[i].Current * (-2.3 / 50);
+                    }
+                    else if (j == 1) {
+                        if (jData.rows[i].Power > 50) {
+                            dialModel[i][j].getObjectByName('Needle').material = redMaterial;
+                            box[i][j].material.color = new THREE.Color(0xbf3030);
+                        } else {
+                            dialModel[i][j].getObjectByName('Needle').material = greenMaterial;
+                            box[i][j].material.color = new THREE.Color(0x5bb73b);
+                        }
+                        dialModel[i][j].getObjectByName('Needle').rotation.y = jData.rows[i].Power * (-2.3 / 50);
+                    }
+                    else if (j == 2) {
+                        if (jData.rows[i].Voltage > 400 && jData.rows[i].Voltage < 500) {
+                            dialModel[i][j].getObjectByName('Needle1').material = greenMaterial;
+                            box[i][j].material.color = new THREE.Color(0x5bb73b);
+                        }
+                        else {
+                            dialModel[i][j].getObjectByName('Needle1').material = redMaterial;
+                            box[i][j].material.color = new THREE.Color(0xbf3030);
+                        }
+                        let dataV = jData.rows[i].Voltage;
+                        if (dataV == 0) dataV = 300;
+                        dialModel[i][j].getObjectByName('Needle1').rotation.y = (dataV - 300) * (-2.3 / 150);
+                    }
+                }
+            }
+        }
+
+        if (dialLoaded) {
+            for (let i = 0; i < planes.length; i++) {
+                if (canedit) {
+                    loadText(jData.rows[i].AssetName, planes[i], 0, .37, 0.2, .05, i, 0, "p" + (i + 1) + "AssetName", whiteC);
+                    loadText("Current Average (A)", planes[i], -.5, .13, 0, .025, i, 1, "p" + (i + 1) + "CurrentText", blackC);
+                    loadText("Active Power (kW)", planes[i], 0, .13, 0, .025, i, 2, "p" + (i + 1) + "PowerText", blackC);
+                    loadText("Line Voltage Average (V)", planes[i], .5, .13, 0, .025, i, 3, "p" + (i + 1) + "VoltageText", blackC);
+                }
+                loadText(jData.rows[i].Current, planes[i], -.5, -.3, 0, .025, i, 4, "p" + (i + 1) + "Current", blackC);
+                loadText(jData.rows[i].Power, planes[i], 0, -.3, 0, .025, i, 5, "p" + (i + 1) + "Power", blackC);
+                loadText(jData.rows[i].Voltage, planes[i], .5, -.3, 0, .025, i, 6, "p" + (i + 1) + "Voltage", blackC);
+            }
+            canedit = false;
+
+        }
     });
-    // const fontLoad = new FontLoader();
-    // fontLoad.load('./fonts/helvetiker_regular.typeface.json', function (font) {
-    //     console.log(dispText)
-    //     const geometry = new TextGeometry(dispText[0], {
-    //         font: font,
-    //         size: .08,
-    //         height: .001
-    //     });
-    //     const materials = [
-    //         new THREE.MeshPhongMaterial({ color: 0x000 }), // front
-    //         new THREE.MeshPhongMaterial({ color: 0x000 }) // side
-    //     ];
-    //     const textMesh1 = new THREE.Mesh(geometry, materials);
-    //     textMesh1.position.set(-.7, .2, 0);
-    //     textMesh1.textAlign = 'center';
-    //     plane1.add(textMesh1)
-    // });
+}
+
+function loadText(textArr, planeName, x, y, z, sz, index, tId, uname, clr) {
+    // plane1Box1.material.color = new THREE.Color(0xbf3030);
+    let textname = uname + tId;
+    const fontLoad = new FontLoader();
+    fontLoad.load('./fonts/helvetiker_regular.typeface.json', function (font) {
+        const geometry = new TextGeometry(textArr, {
+            font: font,
+            size: sz,
+            height: .005
+        });
+        const materials = [
+            new THREE.MeshPhongMaterial({ color: clr }), // front
+            new THREE.MeshPhongMaterial({ color: clr }) // side
+        ];
+        geometry.center();
+        textname = new THREE.Mesh(geometry, materials);
+        textname.position.set(x, y, z);
+        textname.textAlign = 'center';
+        planeName.add(textname)
+
+        textMesh[index][tId] = textname;
+    });
 }
